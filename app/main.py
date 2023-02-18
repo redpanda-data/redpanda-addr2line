ca013659602223eb3af3289e91173a902a4c77f0
Noah Watkins <noahwatkins@gmail.com>
2023-02-18 13:52:43 -0800

    fully functional proof-of-concept decoder webapp
    Primary things missing are authentication and rate limiting if that is
    desired, and automatic removal of old packages according to some policy
    to prevent disks from filling up.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ app/main.py
@@ -0,0 +1,102 @@
+import os
+import pathlib
+import re
+import subprocess
+from fastapi import FastAPI, HTTPException, Path, Body, status
+from fastapi.responses import PlainTextResponse
+
+#
+# Maximum length of a stack trace that the API will accept
+#
+max_trace_length = int(os.getenv("MAX_TRACE_LEN", 2**17))
+assert max_trace_length > 0, f"Max trace length is daft: {max_trace_length}"
+
+#
+# Path to the seastar-addr2line script
+#
+addr2line = pathlib.Path(
+    os.getenv("SEASTAR_ADDR2LINE_PATH", "/usr/src/scripts/seastar-addr2line")
+)
+assert addr2line.is_file(), f"seastar-addr2line tool {addr2line} does not exist"
+
+#
+# Directory containing extracted Redpanda releases
+#
+download_dir = pathlib.Path(os.getenv("DOWNLOAD_DIR", "/mnt/redpanda"))
+assert download_dir.is_dir(), f"download directory {download_dir} does not exist"
+
+app = FastAPI(redoc_url=None, debug=False)
+
+
+@app.get("/versions", summary="Read supported Redpanda versions")
+def versions():
+    versions = []
+    for dir in download_dir.iterdir():
+        if not dir.is_dir():
+            continue
+        if not re.match(r"^\d{2}\.\d\.\d{1,2}$", dir.name):
+            continue
+        versions.append(dir.name)
+    return versions
+
+
+def redpanda_exec_path(version):
+    """
+    Form the path to the redpanda binary for the given version.
+
+    Raises a 404 exception if the resulting path does not point at a file.
+    """
+    redpanda = download_dir / version / "libexec" / "redpanda"
+    if not redpanda.is_file():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Redpanda version not found: {version}",
+        )
+    return redpanda
+
+
+def decode_redpanda_backtrace(trace, version):
+    """
+    Decode a backtrace against the specified version of Redpanda.
+    """
+    redpanda = redpanda_exec_path(version).as_posix()
+    args = f"python {addr2line} -a llvm-addr2line -e {redpanda}"
+    return subprocess.check_output(
+        args.split(), stderr=subprocess.STDOUT, input=trace, text=True
+    )
+
+
+@app.post(
+    "/backtrace/{version}",
+    summary="Decode a backtrace from Redpanda",
+    response_class=PlainTextResponse,
+    response_description="Hey i'm a response desription",
+)
+def backtrace(
+    version: str = Path(
+        title="Redpanda version",
+        default=...,
+        regex=r"^\d{2}\.\d\.\d{1,2}$",
+        example="22.3.11",
+    ),
+    trace: str = Body(
+        media_type="text/plain",
+        min_length=1,
+        max_length=max_trace_length,
+        example="""Backtrace:
+  0x5a3e146
+  0x5aa1aa6
+  /opt/redpanda/lib/libc.so.6+0x42abf
+  0x5a5c27d
+  0x5a5ff57
+  0x5a5d329
+  0x5980d81
+  0x597ee9f
+  0x1d4cfde
+  0x5d7245d
+  /opt/redpanda/lib/libc.so.6+0x2d58f
+  /opt/redpanda/lib/libc.so.6+0x2d648
+  0x1d472a4""",
+    ),
+):
+    return decode_redpanda_backtrace(trace, version)
---
+++ sync/main.py
@@ -0,0 +1,164 @@
+import contextlib
+import os
+import pathlib
+import re
+import requests
+import shutil
+import signal
+import tarfile
+import tempfile
+import threading
+import traceback
+
+#
+# Cloudsmith API key for listing and downloading releases
+#
+api_key = os.getenv("CLOUDSMITH_API_KEY", None)
+assert api_key, "required api key not provided: set CLOUDSMITH_API_KEY"
+
+#
+# Directory used to store extracted Redpanda releases
+#
+download_dir = pathlib.Path(os.getenv("DOWNLOAD_DIR", "/mnt/redpanda"))
+assert download_dir.is_dir(), f"download directory {download_dir} does not exist"
+
+#
+# How often (in minutes) to query for new releases
+refresh_key = "CLOUDSMITH_REFRESH_MINUTES"
+refresh_seconds = 60 * int(os.getenv(refresh_key, 30))
+assert refresh_seconds > 0, "invalid refresh rate: {os.getenv(refresh_key)}"
+
+#
+# Minimum major version that will be considered when syncing
+#
+min_major_version = int(os.getenv("MIN_MAJOR_VERSION", 22))
+
+
+@contextlib.contextmanager
+def cloudsmith_session():
+    """
+    Build a cloudsmith api request sessions with configured api key.
+    """
+    with requests.Session() as sesh:
+        sesh.headers.update(
+            {
+                "Accept": "*/*",
+                "X-Api-Key": api_key,
+            }
+        )
+        yield sesh
+
+
+def list_all_packages():
+    """
+    Fetch all known Redpanda packages.
+    """
+
+    def package_iter(sesh):
+        url = "https://api.cloudsmith.io/v1/packages/redpanda/redpanda/?q=name:redpanda-amd64+format:raw"
+        while url is not None:
+            resp = sesh.get(url)
+            resp.raise_for_status()
+            yield from resp.json()
+            link = resp.links.get("next", None)
+            url = link.get("url", None) if link else None
+
+    with cloudsmith_session() as sesh:
+        return [p for p in package_iter(sesh)]
+
+
+def sync_packages():
+    """
+    List all Redpanda packages eligible to sync.
+    """
+    for pkg in list_all_packages():
+        name = pkg["name"]
+        assert (
+            name == "redpanda-amd64"
+        ), f"Query returned package with unsupported name: {name}"
+        assert (
+            pkg["format"] == "raw"
+        ), f"Query returned package with unsupported format: {pkg['format']}"
+
+        version = pkg["version"]
+        m = re.match(r"^(?P<major>\d{2})\.\d+\.\d{1,2}$", version)
+        if not m:
+            print(f"Skipping package {name} with invalid version: {version}")
+            continue
+        if int(m.group("major")) < min_major_version:
+            print(
+                f"Skipping package {name} with major version {version} < {min_major_version}"
+            )
+            continue
+        if not pkg["is_downloadable"] or not pkg["is_sync_completed"]:
+            print(
+                f"Skipping package {name} not ready: d={pkg['is_downloadable']} s={pkg['is_sync_completed']}"
+            )
+            continue
+
+        yield version, pkg["cdn_url"]
+
+
+def download_package(version, url):
+    """
+    Downloads and extracts a Redpanda release into download directory.
+    """
+    path = download_dir / version
+    if path.is_dir():
+        print(f"Skipping {version} at {path}: already downloaded")
+        return
+
+    print(f"Downloading {version} from {url}")
+    f = tempfile.NamedTemporaryFile(delete=False)
+    try:
+        with cloudsmith_session() as sesh:
+            with sesh.get(url, stream=True) as s:
+                shutil.copyfileobj(s.raw, f)
+    except:
+        os.unlink(f.name)
+        raise
+    finally:
+        f.close()
+
+    # create a temporary directory to unpack release into
+    try:
+        tdir = tempfile.mkdtemp()
+    except:
+        os.unlink(f.name)
+        raise
+
+    print(f"Extracting {version} at {f.name} into {tdir}")
+    try:
+        with tarfile.open(f.name) as tfile:
+            tfile.extractall(path=tdir)
+        redpanda = pathlib.Path(tdir) / "libexec" / "redpanda"
+        assert (
+            redpanda.is_file()
+        ), "Redpanda binary not found in extracted release {version} at location {redpanda.as_posix()}"
+        print(f"Moving extracted {version} from {tdir} to {path}")
+        shutil.move(tdir, path)
+    except:
+        shutil.rmtree(tdir, ignore_errors=True)
+        raise
+    finally:
+        os.unlink(f.name)
+
+
+if __name__ == "__main__":
+    stop = threading.Event()
+    for sig in (signal.SIGTERM, signal.SIGINT):
+        signal.signal(sig, lambda *_: stop.set())
+
+    while not stop.is_set():
+        try:
+            for version, url in sync_packages():
+                download_package(version, url)
+            print(f"Refresh complete. Next refresh in {refresh_seconds} sec")
+            stop.wait(refresh_seconds)
+
+        except Exception:
+            traceback.print_exc()
+            print("Encountered exception. Trying refresh again in 5 minutes")
+            stop.wait(300)
+
+    print("Redpanda release synchronizer stopping")
